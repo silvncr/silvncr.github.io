ff7356ba447aaef73d9bfa501c8b52c84a11425a
JohnnySD <ba004629@bac.qld.edu.au>
2021-06-17 17:52:10 +1000

    Update

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,6 +1,12 @@
-var m1 = function() {
+var m10 = function() {
   alert('Pokemon PythonRed Mystery Gift #10: "TITLE"');
 };
-var m2 = function() {
+var m12 = function() {
   alert('Pokemon PythonRed Mystery Gift #12: "CORRECT"');
+};
+var m18 = function() {
+  alert('Pokemon PythonRed Mystery Gift #18: "SHIMMER"')
+};
+var m20 = function() {
+  alert('Pokemon PythonRed Mystery Gift #20: "POKEMONPYTHONRED"')
 };
