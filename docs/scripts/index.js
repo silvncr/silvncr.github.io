570c75323a5b82698de89bf12b0a3aac401acf69
JohnnySD <johnsdieckmann@gmail.com>
2022-05-05 19:24:53 +1000

    Layout

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,12 +1,12 @@
 var m10 = function() {
-  alert('Pokemon PythonRed Mystery Gift #10: "TITLE"');
+	alert('Pokemon PythonRed Mystery Gift #10: "TITLE"');
 };
 var m12 = function() {
-  alert('Pokemon PythonRed Mystery Gift #12: "CORRECT"');
+	alert('Pokemon PythonRed Mystery Gift #12: "CORRECT"');
 };
 var m18 = function() {
-  alert('Pokemon PythonRed Mystery Gift #18: "SHIMMER"')
+	alert('Pokemon PythonRed Mystery Gift #18: "SHIMMER"')
 };
 var m20 = function() {
-  alert('Pokemon PythonRed Mystery Gift #20: "POKEMONPYTHONRED"')
+	alert('Pokemon PythonRed Mystery Gift #20: "POKEMONPYTHONRED"')
 };
