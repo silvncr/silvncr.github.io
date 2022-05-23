546d3c1fd2d2db15c3d0b81e5358775d1451bf60
JohnnySD <johnsdieckmann@gmail.com>
2022-05-23 20:45:36 +1000

    Rewrite

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 <hr>
-<h1 align="center"><a href="https://turnipguy30.github.io" target="_blank" rel="noopener noreferrer"><code>https://turnipguy30.github.io</code></a></h1>
+<h1 align="center"><a href="https://turnipguy30.github.io/"><code>https://turnipguy30.github.io/</code></a></h1>
 <hr>
-<h6 align="center">WIP</h6>
--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,12 +1,9 @@
+window.onload = function() {
+	document.getElementById("content") = ("https://raw.githubusercontent.com/TurnipGuy30/TurnipGuy30/master/README.md").text()
+}
 var m10 = function() {
 	alert('Pokemon PythonRed Mystery Gift #10: "TITLE"');
-};
+}
 var m12 = function() {
 	alert('Pokemon PythonRed Mystery Gift #12: "CORRECT"');
-};
-var m18 = function() {
-	alert('Pokemon PythonRed Mystery Gift #18: "SHIMMER"')
-};
-var m20 = function() {
-	alert('Pokemon PythonRed Mystery Gift #20: "POKEMONPYTHONRED"')
-};
+}
