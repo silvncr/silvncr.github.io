e844b311cec67d257ed113ce665665f8a5ff16e7
silvncr <johnsdieckmann@gmail.com>
2023-10-04 20:00:13 +1000

    Level 1

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,102 +1,169 @@
 window.onload = () => {
-	let i = 1n,
-		x = 3n * 10n ** 1020n,
-		pi = x;
+	let i = 1n;
+	let x = 3n * 10n ** 1020n;
+	let pi = x;
 	while (x > 0) {
-		x = (x * i) / ((i + 1n) * 4n),
-			pi += x / (i + 2n),
-			i += 2n;
-	};
-	pi_out = (pi / 10n ** 20n).toString().slice(1);
+		x = (x * i) / ((i + 1n) * 4n);
+		pi += x / (i + 2n);
+		i += 2n;
+	}
+	const pi_out = (pi / 10n ** 20n).toString().slice(1);
 	for (const [name, link] of [
 		["Discord", "https://discord.com/invite/kx5cVTWjEg"],
 		["GitHub", "https://github.com/silvncr"],
 		["Instagram", "https://instagram.com/silvncr"],
 		["Spotify", "https://open.spotify.com/artist/6XO0vKydOw2Zt9kI36Xtj7"],
 		["Twitch", "https://twitch.tv/silvncr"],
+		["Twitter", "https://twitter.com/@silvncr"],
 		["YouTube", "https://youtube.com/@silvncr"],
 	]) {
-		document.querySelector(".screen-links").innerHTML += `<a href="${link}" class="${name.toLowerCase()}"><em class="icofont-${name.toLowerCase()}"></em></a><!--${name}-->\n`;
-	};
-	let keys_in = "",
-		pi_game_active = pi_game_prompt = false,
-		pi_game_counter = 0;
-	const change = (vars) => Object.entries(vars).forEach((v) => root.style.setProperty(v[0], v[1])),
-		colours = {
-			blue: {
-				"--primary-1": "33 150 255",
-				"--primary-2": "30 143 255",
-			},
-			cream: {
-				"--primary-1": "245 222 179",
-				"--primary-2": "255 255 160",
-			},
-			green: {
-				"--primary-1": "0 255 0",
-				"--primary-2": "8 230 30",
-			},
-			orange: {
-				"--primary-1": "255 165 0",
-				"--primary-2": "240 180 0",
-			},
-			pink: {
-				"--primary-1": "255 108 203",
-				"--primary-2": "255 182 193",
-			},
-			purple: {
-				"--primary-1": "135 0 255",
-				"--primary-2": "128 0 128",
-			},
-			red: {
-				"--primary-1": "255 0 0",
-				"--primary-2": "255 30 24",
-			},
-			yellow: {
-				"--primary-1": "255 255 0",
-				"--primary-2": "240 240 0",
-			},
+		document.querySelector(
+			".screen-links"
+		).innerHTML += `<a href="${link}" class="${name.toLowerCase()}"><em class="icofont-${name.toLowerCase()}"></em></a><!--${name}-->\n`;
+	}
+	let keys_in = "";
+	let pi_game_active = false;
+	let pi_game_prompt = false;
+	let pi_game_counter = 0;
+	const change = (vars) =>
+		Object.entries(vars).forEach((v) => root.style.setProperty(v[0], v[1]));
+	const alpha = "abcdefghijklmnopqrstuvwxyz";
+	const nums = "0123456789";
+	const colours = {
+		blue: {
+			"--primary-1": "33 150 255",
+			"--primary-2": "30 143 255",
 		},
-		root = document.querySelector(":root"),
-		link = document.querySelector(".link"),
-		link_text = link.innerHTML;
-	change(colours.blue);
-	window.addEventListener("keypress", function (event) {
-		key = event.key.toLowerCase(),
+		cream: {
+			"--primary-1": "245 222 179",
+			"--primary-2": "255 255 160",
+		},
+		cyan: {
+			"--primary-1": "0 255 255",
+			"--primary-2": "0 240 240",
+		},
+		green: {
+			"--primary-1": "0 255 0",
+			"--primary-2": "8 230 30",
+		},
+		none: {
+			"--primary-1": "auto",
+			"--primary-2": "auto",
+		},
+		orange: {
+			"--primary-1": "255 165 0",
+			"--primary-2": "240 180 0",
+		},
+		pink: {
+			"--primary-1": "255 108 203",
+			"--primary-2": "255 182 193",
+		},
+		purple: {
+			"--primary-1": "135 0 255",
+			"--primary-2": "128 0 128",
+		},
+		red: {
+			"--primary-1": "255 0 0",
+			"--primary-2": "255 30 24",
+		},
+		yellow: {
+			"--primary-1": "255 255 0",
+			"--primary-2": "240 240 0",
+		},
+	};
+	const root = document.querySelector(":root");
+	const link = document.querySelector(".link");
+	let link_text_old = link.innerHTML;
+	let link_text = link_text_old;
+	let click_order = "";
+	let vault_in = "";
+	let vault = {
+		0: {
+			active: true,
+			solved: false,
+		},
+		1: {
+			active: false,
+			solved: false,
+		},
+	};
+	change(colours.cyan);
+	window.addEventListener("keydown", (event) => {
+		let key = event.key.toLowerCase();
+		if (
+			alpha.concat(nums).concat([" ", ".", "backspace", "enter"]).includes(key)
+		) {
 			keys_in += key;
-		if (pi_game_prompt) {
-			if (key == '.') {
-				pi_game_active = true,
-					pi_game_prompt = false;
-				link.innerHTML = pi_game_counter;
-			} else {
-				pi_game_prompt = false,
+			if (vault[1].active) {
+				if (key == "enter") {
+					if (vault_in.toLowerCase() == "ptfu1yi35") {
+						vault[1].solved = true;
+						vault[1].active = false;
+						vault[0].active = true;
+						link_text = "welcome back, silvncr";
+						vault_in = "";
+					}
+					vault_in = "";
 					link.innerHTML = link_text;
-			};
-		} else if (pi_game_active) {
-			link.innerHTML = pi_game_counter;
-			if (key == pi_out[pi_game_counter]) {
-				pi_game_counter += 1;
+				} else if (key == "backspace") {
+					vault_in = vault_in.slice(0, -1);
+				} else if (vault_in.length <= 15) {
+					vault_in += key;
+				}
+				link.innerHTML = vault_in || link_text;
+			} else if (pi_game_prompt) {
+				if (key == ".") {
+					pi_game_active = true;
+					pi_game_prompt = false;
+					link.innerHTML = pi_game_counter;
+				} else {
+					pi_game_prompt = false;
+					link.innerHTML = link_text;
+				}
+			} else if (pi_game_active) {
 				link.innerHTML = pi_game_counter;
-			} else if ("0123456789".includes(key)) {
-				link.innerHTML = `you scored: ${pi_game_counter}`,
-					pi_game_active = pi_game_prompt = false,
+				if (key == pi_out[pi_game_counter]) {
+					if (pi_game_counter == pi_out.length) {
+						link.innerHTML = `really, ${pi_out.length} digits?!`;
+						pi_game_active = pi_game_prompt = false;
+						pi_game_counter = 0;
+					} else {
+						pi_game_counter += 1;
+						link.innerHTML = pi_game_counter;
+					}
+				} else if (nums.includes(key)) {
+					link.innerHTML = `you scored: ${pi_game_counter}`;
+					pi_game_active = pi_game_prompt = false;
 					pi_game_counter = 0;
-			};
-		} else {
-			pi_game_active = pi_game_prompt = false,
-				link.innerHTML = link_text;
-			if (keys_in.endsWith("3")) {
-				pi_game_prompt = true,
-					link.innerHTML = "press . to play";
+				}
 			} else {
-				for (const [key, val] of Object.entries(colours)) {
-					if (keys_in.endsWith(key)) {
-						change(val);
-					};
-				};
-			};
-		};
+				pi_game_active = pi_game_prompt = false;
+				link.innerHTML = link_text;
+				if (keys_in.endsWith("3")) {
+					pi_game_prompt = true;
+					link.innerHTML = "press . to play";
+				} else {
+					for (const [key, val] of Object.entries(colours)) {
+						if (keys_in.endsWith(key)) {
+							change(val);
+						}
+					}
+				}
+			}
+		}
 	});
+	link_span_click = (num) => {
+		click_order += num.toString();
+		if (click_order.endsWith("3124")) {
+			click_order = "";
+			link_text = "please enter the password";
+			link.innerHTML = link_text;
+			vault[0].solved = true;
+			vault[0].active = false;
+			vault[1].active = true;
+		}
+	};
+	link_span_click("");
 	window.onpointermove = (event) => {
 		const { clientX, clientY } = event;
 		document.getElementById("blob").animate(
@@ -104,7 +171,10 @@ window.onload = () => {
 				left: `${clientX}px`,
 				top: `${clientY}px`,
 			},
-			{ duration: 3000, fill: "forwards" }
+			{
+				duration: 3000,
+				fill: "forwards",
+			}
 		);
 	};
 	let interval = null;
@@ -118,13 +188,13 @@ window.onload = () => {
 				.map((letter, index) => {
 					if (index < iteration) {
 						return name.dataset.value[index];
-					};
-					return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[Math.floor(Math.random() * 26)];
+					}
+					return alpha[Math.floor(Math.random() * 26)];
 				})
 				.join("");
 			if (iteration >= name.dataset.value.length) {
 				clearInterval(interval);
-			};
+			}
 			iteration += 1 / 3;
 		}, 30);
 	};
